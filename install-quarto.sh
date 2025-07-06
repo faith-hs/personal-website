1a6c7a575ecd5c0fb56c5d778f7ad33136f2a70b
Faith Satrya <faithsatrya17@gmail.com>
2025-07-06 10:33:05 +0700

    Install Quarto locally without sudo and update PATH

--- install-quarto.sh
+++ install-quarto.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# Install Quarto CLI
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
-sudo dpkg -i quarto-${QUARTO_VERSION}-linux-amd64.deb
-quarto check install
+# Exit if anything fails
+set -e
+
+# Download and extract Quarto to local directory
+curl -L -o quarto.tar.gz https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
+tar -xzf quarto.tar.gz
+
+# Add Quarto to the PATH (local install)
+export PATH=$PWD/quarto-${QUARTO_VERSION}/bin:$PATH
+
+# Confirm installation
+quarto --version
+
+# Run Quarto render
+quarto render
